de2f217db2637724d79c0c05ef2facc8675adc70
xtedious <xtediousdev@proton.me>
2025-04-01 17:09:25 +0200

    110 current  2025-03-30 21:48:01  24.11.20250313.cdd2ef0  6.13.6                          *

--- modules/pico-dev.nix
+++ modules/pico-dev.nix
@@ -2,9 +2,18 @@
   lib,
   pkgs,
   config,
-  unstable,
   ...
-}: {
+}: let
+  pico-sdk-200 = pkgs.pico-sdk.overrideAttrs (oldAttrs: rec {
+    src = pkgs.fetchFromGitHub {
+      owner = "raspberrypi";
+      repo = "pico-sdk";
+      rev = "2.0.0";
+      sha256 = "sha256:0h2j9g7z87gqv59mjl9aq7g0hll0fkpz281f4pprhjxww6789abp";
+      fetchSubmodules = true;
+    };
+  });
+in {
   options.pico-dev = {
     enable = lib.mkEnableOption "Enable the Pico Development Environment";
 
@@ -20,7 +29,7 @@
     env-variables = lib.mkOption {
       type = lib.types.attrsOf lib.types.str;
       default = {
-        PICO_SDK_PATH = "${pkgs.pico-sdk}";
+        PICO_SDK_PATH = "${pico-sdk-200}/libs/pico-sdk";
       };
       description = "Environment variables";
     };
@@ -29,8 +38,7 @@
   config = lib.mkIf config.pico-dev.enable {
     environment.systemPackages = with pkgs;
       [
-        unstable.pico-sdk
-        picotool
+        pico-sdk-200
         clang-tools
         python3
         cmake
